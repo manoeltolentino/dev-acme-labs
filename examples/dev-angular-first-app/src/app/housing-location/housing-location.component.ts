51caa7b514be092eb40dfc672a453cfe282997a6
manoeltolentino <manoeltolentino@outlook.com>
2025-02-20 21:19:02 -0300

    Commit

---
+++ examples/dev-angular-first-app/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Housinglocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.scss'
+})
+export class HousingLocationComponent {
+
+  @Input() housingLocation!: Housinglocation;
+
+}
